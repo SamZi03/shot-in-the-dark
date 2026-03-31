891353bb17da90c428d2a2b88e6e224335ec134c
Sam <samsamuman@gmail.com>
2026-03-31 18:41:47 +0100

    Add 500 mixed questions, UI improvements, offline service worker

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'sitd-v1';
+
+const FILES = [
+  './',
+  './index.html',
+  './game.html',
+  './css/style.css',
+  './js/app.js',
+  './js/game.js',
+  './data/history.json',
+  './data/science.json',
+  './data/sport.json',
+  './data/music.json',
+  './data/food.json',
+  './data/geography.json',
+  './data/tv-film.json',
+  './data/mixed.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
